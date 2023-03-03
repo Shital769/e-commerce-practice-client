7cd3b64cb24cea8b0158b44a0d8bf3066ebadf13
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-03-03 23:49:52 +1100

    create a login page

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,16 +1,48 @@
 import React, { useRef, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import { Header } from "../layout/Header";
+import { Footer } from "../layout/Footer";
 import Button from "react-bootstrap/Button";
 import { Spinner } from "react-bootstrap";
-import { Footer } from "../layout/Footer";
-import { Header } from "../layout/Header";
+import { loginAction } from "./AuthAction";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 export const LoginPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate;
+  const emailRef = useRef("");
+  const passRef = useRef("");
+
+  //calling the  updated state from store connected with authSLice
+  const { isLoading, user } = useSelector((state) => state.user);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      email: emailRef.current.value,
+      password: passRef.current.value,
+    };
+
+    //dispatch login action to call an api
+    if (!formData.email || !formData.password) {
+      return alert("Please fill in the both fields");
+    }
+    dispatch(loginAction(formData));
+  };
+
+  useEffect(() => {
+    user?._id && navigate("/dashboard");
+    //TODO: make router private and auto login
+  }, [user, navigate]);
+
   return (
     <div>
       <Header />
 
       <div className="main login-page">
-        <Form className="shadow-lg rounded">
+        <Form className="shadow-lg rounded" onSubmit={handleOnSubmit}>
           <h3 className="textcenter">Welcome!</h3>
           <hr className="mb-5" />
           <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -39,7 +71,7 @@ export const LoginPage = () => {
             )}
           </Button>
           <div className="text-center p-5">
-            Forget Password? <a href="#">Reset Password</a>
+            Forget Password? <a href="/reset-password">Reset Password</a>
           </div>
         </Form>
       </div>
