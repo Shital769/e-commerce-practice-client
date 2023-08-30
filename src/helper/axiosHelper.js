d88fe0f8ebb5e4639a4e985a6e164622d3891f86
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-08-30 23:57:16 +1000

    created fetchprocessor

--- src/helper/axiosHelper.js
+++ src/helper/axiosHelper.js
@@ -2,6 +2,39 @@ import axios from "axios";
 const rootUrl = process.env.REACT_APP_DOMAIN + "api/v1";
 const adminApi = rootUrl + "/admin";
 
+const ftechProcessor = async ({ method, url, data, isPrivate, token }) => {
+  try {
+    const jwtToken = token || sessionStorage.getItem("accessJWT");
+    console.log(jwtToken);
+    const headers = isPrivate
+      ? {
+          Authorization: jwtToken,
+        }
+      : null;
+
+    const res = await axios({
+      method,
+      url,
+      data,
+      headers,
+    });
+    return res.data;
+  } catch (error) {
+    const message = error.message;
+
+    if (error?.response?.data?.message === "jwt expired") {
+      const { accessJWT } = await fetchNewAccessJWT();
+      sessionStorage.setItem("accessJWT", accessJWT);
+      return fetchProcesser({ method, url, data, isPrivate, token: accessJWT });
+    }
+
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
+
 export const postNewAdmin = async (data) => {
   try {
     const res = await axios.post(adminApi + "/register", data);
