ebfd60bfd22d381fcf6a9eca462175598f4695fb
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-09-06 23:22:12 +1000

    updated store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./pages/login/AuthSlice";
+import systemReducer from "./system/systemSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: authReducer,
+    system: systemReducer,
+  },
 });
+
+export default store;
